93873b179338c2353b75a0093100810958ccf29a
Muresan Ioan-Alexandru <muresanalex3@yahoo.com>
2022-03-16 01:42:05 +0200

    Create labsd_destination.sql

---
+++ labsd_destination.sql
@@ -0,0 +1,8 @@
+INSERT INTO labsd.destination (id, name) VALUES ('10', 'Baritiu D21');
+INSERT INTO labsd.destination (id, name) VALUES ('12', 'USA');
+INSERT INTO labsd.destination (id, name) VALUES ('13', 'Miami');
+INSERT INTO labsd.destination (id, name) VALUES ('14', 'Beijing');
+INSERT INTO labsd.destination (id, name) VALUES ('6', 'Costa Rica');
+INSERT INTO labsd.destination (id, name) VALUES ('7', 'Eforie Nord');
+INSERT INTO labsd.destination (id, name) VALUES ('8', 'Mamaia');
+INSERT INTO labsd.destination (id, name) VALUES ('9', 'Bali');
